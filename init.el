acd151c0da10ad7d08e7c365462dc46774282320
PolarisJunior <brownasianpj@yahoo.com>
2018-12-02 01:46:06 -0800

    new theme, keybind for comment

--- init.el
+++ init.el
@@ -13,6 +13,27 @@
   ;;   t)
   (package-initialize))
 
+(when (not package-archives)
+  (package-refresh-contents))
+
+(defvar my-packages
+  '(material-theme))
+
+(menu-bar-mode -1)
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      my-packages)
+
+(global-company-mode)
+
 (defun copy-all ()
   (interactive)
   (progn
@@ -33,12 +54,22 @@ Version 2015-08-22"
       (kill-new (buffer-string))
       (message "Buffer content copied."))))
 
-(global-set-key (kbd "C-c a") 'xah-copy-all-or-region)
+(defun testificate ()
+  (interactive)
+  (progn
+    (message "fizz buzz")))
 
-(setq initial-frame-alist '((top . 0) (left . 0) (width . 80) (height . 35))
+
+(global-set-key (kbd "C-c a") 'xah-copy-all-or-region)
+(global-set-key (kbd "C-c w") 'overwrite-mode)
+(global-set-key (kbd "C-;") 'comment-line)
+
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 170) (height . 40))
       org-log-done 'time
       tab-width 2
       js-indent-level 2)
+
+;(global-linum-mode t)
 
 
 (add-to-list 'auto-mode-alist '("\\.ash\\'" . c-mode))
@@ -55,6 +86,7 @@ Version 2015-08-22"
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'intero-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'racket-mode-hook 'paredit-mode)
 
 ;(add-to-list 'load-path
 ;             "~/path-to-yasnippet")
@@ -65,6 +97,9 @@ Version 2015-08-22"
 ;(use-package ensime
 ;  :ensure t
 ;  :pin melpa-stable)
+
+(setq inhibit-startup-message t)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -78,11 +113,12 @@ Version 2015-08-22"
  '(cua-normal-cursor-color "#839496")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (monokai)))
+ '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "93f9654f91d31e9a9ec6ea2fcffcfcab38353a9588673f2b750e591f704cd633" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(fci-rule-color "#383838")
+ '(global-auto-revert-mode t)
  '(haskell-interactive-popup-errors nil)
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
@@ -113,6 +149,9 @@ Version 2015-08-22"
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-agenda-files nil)
+ '(package-selected-packages
+   (quote
+    (racket-mode paredit company solarized-theme clojure-mode cider lua-mode rust-mode xahk-mode ahk-mode monokai-theme use-package auto-complete)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -156,3 +195,4 @@ Version 2015-08-22"
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(load-theme 'material t)
